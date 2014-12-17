6b75a5f60f15b448aa7a435fb18383936719a797
Andrew Au <cshung@gmail.com>
2014-12-17 09:40:50 -0800

    Bug fix - Jobs was not downloaded correctly

--- JwBibleDownloader/JwBibleDownloader/Program.cs
+++ JwBibleDownloader/JwBibleDownloader/Program.cs
@@ -12,6 +12,11 @@
 
     internal static class Program
     {
+        // For debugging use - that can used to download a particular chapter for verification
+        //private static int debugGroupId = 0;
+        //private static int debugBookId = 0;
+        //private static int debugChapterId = 0;
+
         private static void Main(string[] args)
         {
             MainAsync().Wait();
@@ -20,32 +25,47 @@
         private static async Task MainAsync()
         {
             // TODO: Replace it with your language
-            // string bookLink = "http://www.jw.org/en/publications/bible/nwt/books/";
-            string bookLink = "http://www.jw.org/zh-hans/%E5%87%BA%E7%89%88%E7%89%A9/%E5%9C%A3%E7%BB%8F/nwt/%E5%9C%A3%E7%BB%8F%E7%BB%8F%E5%8D%B7/";
+            string bookLink = "http://www.jw.org/en/publications/bible/nwt/books/";
+            // string bookLink = "http://www.jw.org/zh-hans/%E5%87%BA%E7%89%88%E7%89%A9/%E5%9C%A3%E7%BB%8F/nwt/%E5%9C%A3%E7%BB%8F%E7%BB%8F%E5%8D%B7/";
             Bible bible = new Bible();
             bible.Groups = new List<Group>();
             using (HttpClient client = new HttpClient())
             {
-                client.Timeout = TimeSpan.FromSeconds(10);
+                client.Timeout = TimeSpan.FromSeconds(1);
                 var result = await client.GetAsync(bookLink);
                 HtmlDocument indexPageDocument = new HtmlDocument();
                 indexPageDocument.LoadHtml(await result.Content.ReadAsStringAsync());
                 foreach (var group in new string[] { "hebScr", "grkScr" })
                 {
+                    //if (++debugGroupId != 1)
+                    //{
+                    //    continue;
+                    //}
+
                     Group currentGroup = new Group();
                     currentGroup.Books = new List<Book>();
                     bible.Groups.Add(currentGroup);
                     var sprites = indexPageDocument.DocumentNode.SelectNodes("//div[@class='booksGrouping " + group + "']//span[@class='accordionSprite']");
                     foreach (var sprite in sprites)
                     {
+                        //if ((++debugBookId) != 18)
+                        //{
+                        //    continue;
+                        //}
+
                         Book currentBook = new Book();
                         currentBook.Chapters = new List<Chapter>();
                         currentGroup.Books.Add(currentBook);
                         var bookName = sprite.PreviousSibling.InnerText;
                         currentBook.Name = bookName;
                         Console.WriteLine(bookName);
-                        foreach (var chaptersNode in sprite.ParentNode.ParentNode.NextSibling.NextSibling.ChildNodes.First(n=>n.Name == "ul").ChildNodes.Where(lc => lc.Name == "li"))
+                        foreach (var chaptersNode in sprite.ParentNode.ParentNode.NextSibling.NextSibling.ChildNodes.First(n => n.Name == "ul").ChildNodes.Where(lc => lc.Name == "li"))
                         {
+                            //if ((++debugChapterId) != 34)
+                            //{
+                            //    continue;
+                            //}
+
                             string chaptersName = chaptersNode.ChildNodes[0].InnerText;
                             Console.WriteLine(chaptersName);
                             Chapter currentChapter = new Chapter();
@@ -68,24 +88,27 @@
                                         verseNumber++;
                                         StringBuilder verseBuilder = new StringBuilder();
                                         verseBuilder.Append(verseNumber);
-                                        foreach (var node in verse.ChildNodes[0].ChildNodes)
+                                        foreach (var verseNode in verse.ChildNodes)
                                         {
-                                            if (node.Name == "sup" && node.Attributes["class"] != null && node.Attributes["class"].Value == "verseNum")
+                                            foreach (var node in verseNode.ChildNodes)
                                             {
-                                                continue;
-                                            }
+                                                if (node.Name == "sup" && node.Attributes["class"] != null && node.Attributes["class"].Value == "verseNum")
+                                                {
+                                                    continue;
+                                                }
 
-                                            if (node.Name == "span" && node.Attributes["class"] != null && node.Attributes["class"].Value == "chapterNum")
-                                            {
-                                                continue;
-                                            }
+                                                if (node.Name == "span" && node.Attributes["class"] != null && node.Attributes["class"].Value == "chapterNum")
+                                                {
+                                                    continue;
+                                                }
 
-                                            if (node.Name == "a" && node.Attributes["class"] != null && node.Attributes["class"].Value == "xrefLink jsBibleLink")
-                                            {
-                                                continue;
-                                            }
+                                                if (node.Name == "a" && node.Attributes["class"] != null && node.Attributes["class"].Value == "xrefLink jsBibleLink")
+                                                {
+                                                    continue;
+                                                }
 
-                                            verseBuilder.Append(node.InnerText);
+                                                verseBuilder.Append(node.InnerText);
+                                            }
                                         }
                                         currentChapter.Verses.Add(verseBuilder.ToString());
                                     };
